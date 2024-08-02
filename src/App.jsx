982ce048dbd6c612b59f0d1215a729f5a41e821d
Md Mahbub Alom <mdmahbuba034@gmail.com>
2024-08-02 14:51:20 +0600

    project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 
-const App = () => {
+function App() {
+  const [quote, setQuote] = useState("");
+  const [fetchTrigger, setFetchTrigger] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+        );
+        const result = await response.json();
+        setQuote(`"${result[0]}"`);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [fetchTrigger]);
+
+  const handleButtonClick = () => {
+    setFetchTrigger(!fetchTrigger);
+  };
+
   return (
-    <div>
-      
+    <div className="card bg-base-100 my-5 w-96 mx-auto shadow-xl">
+      <h1 className="text-2xl mx-auto">Here is some important Quote: </h1>
+      <div className="card-body">
+        <h2 className="card-title">{quote ? quote : "Loading..."}</h2>
+        <div className="card-actions justify-end">
+          <button onClick={handleButtonClick} className="btn btn-outline btn-accent">
+            New Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
